52056160a44b307879cba8d1f70723f99fd78174
Mannat <sharmamannat.25.11@gmail.com>
2021-08-01 18:10:47 +0530

    Update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MainGame from './components/MainGame';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <h1>2 0 4 8</h1>
+      <h1>GAME</h1></header>
+      <MainGame />
+    </div>
+  );
+}
+
+export default App;
